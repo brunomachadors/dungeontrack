33949ec92cb88a873a4ba9d24983d4dabe2633d3
Bruno Machado <brunomachadors@gmail.com>
2026-03-27 16:16:37 +0000

    change footer background color

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,30 +1,24 @@
-import LinkButton from "../LinkButtons";
+import LinkButton from '../LinkButtons';
 
 export default function Footer() {
   return (
-    <footer className="w-full text-amber-950 bg-amber-100 p-8 flex justify-between">
+    <footer className="w-full text-amber-100 bg-amber-950 p-8 flex justify-between">
       {/* Left side */}
       <div className="flex font-bold p-4 text-center">
         🎲 DungeonTrack {new Date().getFullYear()}
       </div>
 
       {/* Right side */}
-      <div className="flex gap-4 p-4">
+      <section className="flex gap-4 p-4">
         <LinkButton
           href="https://github.com/brunomachadors/dungeontrack"
           text="Github"
           external
         ></LinkButton>
-        <LinkButton
-          href="/Documentation"
-          text="Documentation"
-        ></LinkButton>
+        <LinkButton href="/Documentation" text="Documentation"></LinkButton>
 
-        <LinkButton
-          href="/Contact"
-          text="Contact"
-        ></LinkButton>
-      </div>
+        <LinkButton href="/Contact" text="Contact"></LinkButton>
+      </section>
     </footer>
   );
 }
--- app/components/LinkButtons/index.tsx
+++ app/components/LinkButtons/index.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 type LinkButtonProps = { href: string; external?: boolean; text: string };
 
@@ -6,8 +6,8 @@ function LinkButton({ href, text, external = false }: LinkButtonProps) {
   return (
     <Link
       href={href}
-      target={external ? "_blank" : undefined}
-      className=" text-amber-950 hover:text-amber-100 hover:bg-amber-950 p-2 border rounded "
+      target={external ? '_blank' : undefined}
+      className=" text-amber-100 hover:text-amber-950 hover:bg-amber-100 p-2 border rounded "
     >
       {text}
     </Link>
